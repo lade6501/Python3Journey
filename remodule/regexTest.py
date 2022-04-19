3c7bc0ecd77ea8ff9c4cac3263ea43e46ed1312b
lade6501 <lade6501@gmail.com>
2022-04-19 17:37:22 +0530

    variable name change and text only pattern tested

--- remodule/regexTest.py
+++ remodule/regexTest.py
@@ -11,13 +11,18 @@ sapmle_file = open('./sample.txt','r')
 sample_text = sapmle_file.read()
 
 #extracting all the numbers from sample_text 
-pattern = re.compile(r'[0-9]+')
-result = pattern.findall(sample_text)
+pattern_for_numbers = re.compile(r'[0-9]+')
+result = pattern_for_numbers.findall(sample_text)
 print(result)
 
 #extracting all the 2 digit numbers from sample_text 
-pattern = re.compile(r'\b[0-9]{2}\b')
-result = pattern.findall(sample_text)
+pattern_for_two_digit_numbers = re.compile(r'\b[0-9]{2}\b')
+result = pattern_for_two_digit_numbers.findall(sample_text)
+print(result)
+
+#extracting text only
+patter_textonly = re.compile(r'[^0-9]+')
+result = patter_textonly.findall(sample_text)
 print(result)
 
 #closing a file 
